d7a7d92f770608c6e8bea5740c76ae46aa9efa59
konomae <konomae@users.noreply.github.com>
2014-09-01 17:47:09 +0900

    Sample code

--- Example/DWGNicoVideoExtractor/DWGViewController.m
+++ Example/DWGNicoVideoExtractor/DWGViewController.m
@@ -6,6 +6,8 @@
 //  Copyright (c) 2014 konomae. All rights reserved.
 //
 
+#import <MediaPlayer/MediaPlayer.h>
+#import <DWGNicoVideoExtractor/DWGNicoVideoExtractor.h>
 #import "DWGViewController.h"
 
 @interface DWGViewController ()
@@ -17,6 +19,18 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    
+    [DWGNicoVideoExtractor fetchVideoURLFromID:@"sm23538930" completion:^(NSURL *videoURL, NSError *error) {
+        if (error) {
+            NSLog(@"%@", error);
+            return;
+        }
+        
+        MPMoviePlayerViewController *vc = [[MPMoviePlayerViewController alloc] initWithContentURL:videoURL];
+        [vc.moviePlayer prepareToPlay];
+        [vc.moviePlayer play];
+        [self presentMoviePlayerViewControllerAnimated:vc];
+    }];
 }
 
 @end
